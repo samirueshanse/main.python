6c744681a3ac22c6c5cc7a43e21c8de54b4f290e
samirueshanse <96760990+samirueshanse@users.noreply.github.com>
2022-01-29 22:09:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ bot = telebot.TeleBot("5223814916:AAFFbc5LR94uaRh8lXCDrudY-uX1IyXbbHQ")
 def send_welcome(message):
     bot.reply_to(message,"Hello! I am Management Chat BOT")
 
-@bot.message_handler(commands=["how"])
+@bot.message_handler(commands=["group"])
 def send_welcome(message):
     bot.reply_to(message,"https://t.me/Anytime_Sri_Lankan_link_Share")
 
-@bot.message_handler(commands=["welcome"])
+@bot.message_handler(commands=["newbot"])
 def send_welcome(message):
-    bot.reply_to(message,"Hello {username}")
+    bot.reply_to(message,"Go to the Botfather. And create the bot https://t.me/BotFather")
 
 bot.polling()
